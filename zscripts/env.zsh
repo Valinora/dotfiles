ffcb5614e8c2f0e660c9984feed1ac2e1455c9ab
Colin Jamison <cjamison@valmagus.com>
2022-12-29 09:08:45 -0800

    clean up PATH, remove old rust workaround

--- zscripts/env.zsh
+++ zscripts/env.zsh
@@ -2,12 +2,6 @@
 
 export EDITOR="nvim"
 export SUDO_EDITOR="nvim"
-export PATH="$PATH:/home/nilocshot/bin:/home/nilocshot/.cargo/bin:/home/nilocshot/.local/bin"
-
-env_hook_chpwd() {
-  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust"
-}
-
-add-zsh-hook chpwd env_hook_chpwd
+export PATH="$PATH:$HOME/bin:$HOME/.cargo/bin:$HOME/.local/bin"
 
 alias svim="sudoedit"
