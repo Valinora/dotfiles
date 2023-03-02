1a1bba59606d1ed90b1d4b61302758b08efa50da
Colin Jamison <cjamison@valmagus.com>
2023-03-01 18:18:24 -0800

    Move plugin list to separate file. TODO: Move all setup to plugins.lua

---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,83 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        theme = 'onedark',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      }
+    }
+  },
+  {'navarasu/onedark.nvim', config = true},
+
+  {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v1.x',
+  dependencies = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},         -- Required
+    {'hrsh7th/cmp-nvim-lsp'},     -- Required
+    {'hrsh7th/cmp-buffer'},       -- Optional
+    {'hrsh7th/cmp-path'},         -- Optional
+    {'saadparwaiz1/cmp_luasnip'}, -- Optional
+    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},             -- Required
+    {'rafamadriz/friendly-snippets'}, -- Optional
+  }
+  },
+
+  {
+    'jay-babu/mason-null-ls.nvim',
+    dependencies = {
+      {'jose-elias-alvarez/null-ls.nvim', config = true},
+    'nvim-lua/plenary.nvim'
+    },
+    opts = {
+      automatic_setup = true
+    }
+  },
+
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = 'nvim-lua/plenary.nvim'
+  },
+
+  {
+    'folke/trouble.nvim',
+    opts = {}
+  },
+
+  {
+    'utilyre/barbecue.nvim',
+    dependencies = {
+      'SmiteshP/nvim-navic',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = true
+  },
+
+  'mattn/emmet-vim',
+  {'lewis6991/gitsigns.nvim', dependencies = 'nvim-lua/plenary.nvim', opts = true},
+  'ap/vim-css-color',
+  'nvim-tree/nvim-web-devicons',
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = {
+      view = {
+        side = "right"
+      }
+    }
+  },
+  'Raimondi/delimitMate',
+  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  'jlanzarotta/bufexplorer',
+}
