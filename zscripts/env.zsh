28f0ffa9624801ea78e3bcb0cc05a63b30bee2fb
Colin Jamison <cjamison@valmagus.com>
2023-03-03 09:33:57 -0800

    Screw it, just migrate to LunarVim

--- zscripts/env.zsh
+++ zscripts/env.zsh
@@ -4,5 +4,10 @@ export EDITOR="nvim"
 export SUDO_EDITOR="nvim"
 export PATH="$PATH:$HOME/bin:$HOME/.cargo/bin:$HOME/.local/bin"
 
+if [[ ! -a ~/.local/bin/lvim ]]; then
+  # TODO: Re-write this to run an install prompt on first use of the command.
+  bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+fi
+
 alias svim="sudoedit"
-alias vim="nvim"
+alias vim="lvim"
