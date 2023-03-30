6640e0c355b4ac600a28286bf9f7c23afcd15845
Colin Jamison <cjamison@valmagus.com>
2023-03-30 11:58:57 -0700

    add ssh agent

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,6 +3,7 @@
 export EDITOR="lvim"
 export SUDO_EDITOR="lvim"
 export PATH="$PATH:$HOME/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.scripts/bin"
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gcr/ssh"
 
 alias svim="sudoedit"
 alias vim="lvim-wrapper.sh"
