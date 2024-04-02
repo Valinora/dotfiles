119359f1776b9cd1d29f81121d68dbad1ff0a5ad
Colin Jamison <cjamison@valmagus.com>
2024-04-02 13:45:53 -0700

    Add fish configs for evaluation purposes.

---
+++ config/fish/conf.d/env.fish
@@ -0,0 +1,8 @@
+export EDITOR="lvim"
+export SUDO_EDITOR="lvim"
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$PATH:$HOME/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.scripts/bin:$HOME/.ghcup/bin:$HOME/.deno/bin:$BUN_INSTALL/bin"
+export DENO_INSTALL="$HOME/.deno"
+
+alias svim="sudoedit"
+alias vim="lvim-wrapper.sh"
