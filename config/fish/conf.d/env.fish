c560f76601e28e8d91516d4a6b876e78c105863e
Colin Jamison <cjamison@valmagus.com>
2024-04-30 12:31:36 -0700

    Update env: Add nix output dir to PATH, and setup a ssh keyagent

--- config/fish/conf.d/env.fish
+++ config/fish/conf.d/env.fish
@@ -1,8 +1,10 @@
 export EDITOR="lvim"
 export SUDO_EDITOR="lvim"
 export BUN_INSTALL="$HOME/.bun"
-export PATH="$PATH:$HOME/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.scripts/bin:$HOME/.ghcup/bin:$HOME/.deno/bin:$BUN_INSTALL/bin"
+export PATH="$PATH:$HOME/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.scripts/bin:$HOME/.ghcup/bin:$HOME/.deno/bin:$BUN_INSTALL/bin:$HOME/.nix-profile/bin"
 export DENO_INSTALL="$HOME/.deno"
+export SSH_ASKPASS="/usr/bin/ksshaskpass"
+export SSH_ASKPASS_REQUIRE="prefer"
 
 alias svim="sudoedit"
 alias vim="lvim-wrapper.sh"
