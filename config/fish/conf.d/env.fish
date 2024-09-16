ef5d0f9f6c6f0141f5c3eaa41d70b5cc09cc93ed
Colin Jamison <cjamison@valmagus.com>
2024-09-16 12:26:12 -0700

    Add GIT_ASKPASS env variable

--- config/fish/conf.d/env.fish
+++ config/fish/conf.d/env.fish
@@ -4,6 +4,7 @@ export BUN_INSTALL="$HOME/.bun"
 export PATH="$PATH:$HOME/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.scripts/bin:$HOME/.ghcup/bin:$HOME/.deno/bin:$BUN_INSTALL/bin:$HOME/.nix-profile/bin"
 export DENO_INSTALL="$HOME/.deno"
 export SSH_ASKPASS="/usr/bin/ksshaskpass"
+export GIT_ASKPASS="/usr/bin/ksshaskpass"
 export SSH_ASKPASS_REQUIRE="prefer"
 
 alias svim="sudoedit"
